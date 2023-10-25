086cfd50a50e6b8485cc644a69d1ddbea4dce4c6
deborapeixotoda <134522826+deborapeixotoda@users.noreply.github.com>
2023-10-25 11:24:44 -0300

    tocasompom

--- main.js
+++ main.js
@@ -2,10 +2,3 @@ alert('hello world!')
 function tocaSomPom(){
     document.querySelector('#som_toca_pom').play();
 }
-
-document.querySelector('.tecla_clap').onclick = tocaSomPom;
-function tocaSomClap(){
-    document.querySelector('#som_toca_clap').play();
-}
-
-document.querySelector('.tecla_clap').onclick = tocaSomClap;
