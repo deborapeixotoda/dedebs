d08968a3d11e6c35ba687f9c1675225d31591c7c
deborapeixotoda <134522826+deborapeixotoda@users.noreply.github.com>
2023-10-25 12:08:45 -0300

    funciona pfv

--- main.js
+++ main.js
@@ -33,3 +33,12 @@ function tocaSomPsh(){
 }
 document.querySelector('.tecla_psh').onclick = tocaSomPsh;
 
+function tocaSomTic(){
+    document.querySelector('#som_tecla_tic').play();
+}
+document.querySelector('.tecla_tic').onclick = tocaSomTic;
+
+function tocaSomTom(){
+    document.querySelector('#som_tecla_tom').play();
+}
+document.querySelector('.tecla_tom').onclick = tocaSomTom;
