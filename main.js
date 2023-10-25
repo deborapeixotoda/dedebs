0caf7430ca29c07891890d22090dffe84b23df2d
deborapeixotoda <134522826+deborapeixotoda@users.noreply.github.com>
2023-10-25 11:24:05 -0300

    tocaSOMpom

--- main.js
+++ main.js
@@ -3,4 +3,9 @@ function tocaSomPom(){
     document.querySelector('#som_toca_pom').play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+document.querySelector('.tecla_clap').onclick = tocaSomPom;
+function tocaSomClap(){
+    document.querySelector('#som_toca_clap').play();
+}
+
+document.querySelector('.tecla_clap').onclick = tocaSomClap;
