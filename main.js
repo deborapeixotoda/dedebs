c3c68db93fd538744445f5026ac0c4007b247194
deborapeixotoda <134522826+deborapeixotoda@users.noreply.github.com>
2023-10-25 11:44:51 -0300

    agora vai

--- main.js
+++ main.js
@@ -1,29 +1,9 @@
 function tocaSomPom(){
-    document.querySelector('#som_toca_pom').play();
+    document.querySelector('#som_tecla_pom').play();
 }
 document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
 function tocaSomClap(){
-    document.querySelector('#som_toca_clap').play();
+    document.querySelector('#som_tecla_clap').play();
 }
 document.querySelector('.tecla_clap').onclick = tocaSomClap;
-
-function tocaSomTim(){
-    document.querySelector('#som_toca_tim').play();
-}
-document.querySelector('.tecla_tim').onclick = tocaSomTim;
-
-function tocaSomPuff(){
-    document.querySelector('#som_toca_puff').play();
-}
-document.querySelector('.tecla_puff').onclick = tocaSomPuff;
-
-function tocaSomSplash(){
-    document.querySelector('#som_toca_splash').play();
-}
-document.querySelector('.tecla_splash').onclick = tocaSomSplash;
-
-function tocaSomToim(){
-    document.querySelector('#som_toca_toim').play();
-}
-document.querySelector('.tecla_toim').onclick = tocaSomToim;
